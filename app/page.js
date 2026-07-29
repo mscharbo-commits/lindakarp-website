ebb835a76f35d74e7a18fe699f43e8e1b994af3e
Mark Scharbo <mscharbo@gmail.com>
2026-07-29 01:33:54 +0000

    Restore state clearing on quiz open - fixes age value retention bug

--- app/page.js
+++ app/page.js
@@ -14,6 +14,9 @@ export default function Home() {
   }
 
   const handleOpenQuiz = (quizType) => {
+    setQuizAnswers({})
+    setQuizResults(null)
+    setAiAssessment(null)
     setShowQuiz(quizType)
   }
 
